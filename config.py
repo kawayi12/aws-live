4e16bff40f5c0121a6c8ec297d639cfb263c57cb
kawayi12 <126339546+kawayi12@users.noreply.github.com>
2023-12-20 08:39:57 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.c56symiae14a.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanzhiyi-employee"
+customregion = "us-east-1"
+
